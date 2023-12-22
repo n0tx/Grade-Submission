29d1c71a2939f98e1751d17431afaf3dcc98496e
n0tx <rcandra91@msn.com>
2023-12-22 14:01:44 +0700

    Initialize commit

---
+++ grade-submission-client/src/main/java/com/riki/gradesubmission/client/GradeSubmissionClientApplication.java
@@ -0,0 +1,13 @@
+package com.riki.gradesubmission.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeSubmissionClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeSubmissionClientApplication.class, args);
+	}
+
+}
---
+++ grade-submission-client/src/test/java/com/riki/gradesubmission/client/GradeSubmissionClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riki.gradesubmission.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeSubmissionClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
